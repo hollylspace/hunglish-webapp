135927a9d90c6f7ca8bcd96c7483a56b4445f0c8
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2009-11-26 15:44:45 +0000

    issue 22

--- trunk/index.sh
+++ trunk/index.sh
@@ -1,8 +1,18 @@
-if [ -d $directory ]; then
-	rm -rf index_copy/*
+if [ -d index ]; then
 else 
 	mkdir index_copy
 fi 
+if [ -d index_copy ]; then
+	rm -rf index_copy/*
+else 
+	mkdir index_copy
+fi
+if [ -d index ]; then
+	rm -rf index/*
+else 
+fi 
+
+ 
 cp index/* index_copy/
 
 CLASSPATH="output/hunglishdict-1.0.jar"
