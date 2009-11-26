f41424f89eeb105a5ff16bb1d73eced4f86cc6d4
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2009-11-26 15:29:25 +0000

    issue 22

--- trunk/index.sh
+++ trunk/index.sh
@@ -1,5 +1,11 @@
-CLASSPATH="output/hunglishdict-1.0.jar"
+if [ -d $directory ]; then
+	rm -rf index_copy/*
+else 
+	mkdir index_copy
+fi 
+cp index/* index_copy/
 
+CLASSPATH="output/hunglishdict-1.0.jar"
 
 for jar in lib/*.jar 
 do
@@ -8,3 +14,4 @@ done
 echo $CLASSPATH
 export CLASSPATH
 java -Xmx512m  -classpath $CLASSPATH mokk.nlp.irutil.IndexerConsole $*
+
