b64657e5ffdba21947a0fe315734e27cb15d7a6b
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2010-02-24 17:38:02 +0000

    Issue 30 fileupload  - SQL create statements

--- src/sql/create.sql
+++ src/sql/create.sql
@@ -13,6 +13,7 @@ DROP TABLE IF EXISTS `upload`;
 CREATE TABLE  `upload` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `source_id` bigint(20) NOT NULL,
+  `is_processed` varchar(1) default 'N',
   `author` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `hu_file_path` varchar(255) default NULL,
@@ -20,7 +21,8 @@ CREATE TABLE  `upload` (
   `hu_sentence` varchar(4000) default NULL,
   `en_sentence` varchar(4000) default NULL,
   PRIMARY KEY (`id`),
-  KEY `upload_fki_source` (`source_id`),
+  KEY `upload_fk_source` (`source_id`),
+  KEY `upload_i_is_processed` (`is_processed`),
   CONSTRAINT `upload_fki_source` FOREIGN KEY (`source_id`) REFERENCES `source` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
