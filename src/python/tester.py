1528d7f1f2bc932ae385ee94894e5296d7f9b12c
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2010-03-02 19:02:26 +0000

    Issue 30

--- src/python/tester.py
+++ src/python/tester.py
@@ -3,20 +3,39 @@
 # import MySQL module
 import MySQLdb
 
-# connect
-db = MySQLdb.connect(host="localhost", user="hunglish", passwd="", db="hunglishwebapp")
+'''file -> array holding the lines of the file'''
+def readfile(name):
+    # Open the input files and read lines
+    infile = file(name, 'r')
+    return map( lambda s : s.strip("\n"), infile.readlines() )
 
-# create a cursor
-cursor = db.cursor()
+def testDBConn(username, password, database):
+    # connect
+    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database)
+    
+    # create a cursor
+    cursor = db.cursor()
+    
+    # execute SQL statement
+    cursor.execute("SELECT * FROM source")
+    
+    # get the resultset as a tuple
+    result = cursor.fetchall()
+    
+    # iterate through resultset
+    for record in result:
+        print record[0] , "-->", record[1]
 
-# execute SQL statement
-cursor.execute("SELECT * FROM source")
 
-# get the resultset as a tuple
-result = cursor.fetchall()
+def main() :
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+    else:
+        print 'usage: ***.py [username] [passwd] [db]'
+        sys.exit(-1)
 
-# iterate through resultset
-for record in result:
-    print record[0] , "-->", record[1]
+if __name__ == "__main__" :
+    main()
 
-#hehehe
