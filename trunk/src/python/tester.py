eb3541dc0d3c4f325b190953b0747c75beb89829
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2010-03-02 18:22:15 +0000

    Issue 30 fileupload

---
+++ trunk/src/python/tester.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# import MySQL module
+import MySQLdb
+
+# connect
+db = MySQLdb.connect(host="localhost", user="hunglish", passwd="sw6x2the", db="hunglishwebapp")
+
+# create a cursor
+cursor = db.cursor()
+
+# execute SQL statement
+cursor.execute("SELECT * FROM source")
+
+# get the resultset as a tuple
+result = cursor.fetchall()
+
+# iterate through resultset
+for record in result:
+    print record[0] , "-->", record[1]
