59bb83771b5a433bce8388155fe24a79fb028d5f
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2010-04-27 17:22:11 +0000

    index from database with duplicate filter supported

--- src/test/java/hu/mokk/hunglish/lucene/HungarianLuceneTester.java
+++ src/test/java/hu/mokk/hunglish/lucene/HungarianLuceneTester.java
@@ -45,66 +45,85 @@ public class HungarianLuceneTester {
 	public static String resouceEncodingEn = "ISO-8859-1";
 	public static String resouceEncodingHu = "ISO-8859-2";
 
-	private static String huAff =         "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/mispellRC1.aff";
-	private static String huDic =         "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/mispellRC1.dic";
-	private static String huDerivatives = "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/derivatives.lst";
-	private static String huCompounds =   "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/compound.lst";
-	private static int huRecursionDepth = 4;
-	private static String enAff =         "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/en.aff";
-	private static String enDic =         "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/en.dic";
-	private static int enRecursionDepth = 2;
+	/*
+	 * private static String huAff =
+	 * "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/mispellRC1.aff"
+	 * ; private static String huDic =
+	 * "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/mispellRC1.dic"
+	 * ; private static String huDerivatives =
+	 * "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/derivatives.lst"
+	 * ; private static String huCompounds =
+	 * "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/compound.lst"
+	 * ; private static int huRecursionDepth = 4; private static String enAff =
+	 * "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/en.aff"
+	 * ; private static String enDic =
+	 * "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/jmorph/en.dic"
+	 * ; private static int enRecursionDepth = 2;
+	 */
 
-	//private static String testDataSenHu = "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/test-data-sen.txt";
-	private static String testDataQueryHu = "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/test-data.txt";	
+	private static String huAff = "C:\\workspaces\\hunglish-webapp\\trunk\\data\\jmorph\\mispellRC1.aff";
+	private static String huDic = "C:\\workspaces\\hunglish-webapp\\trunk\\data\\jmorph\\mispellRC1.dic";
+	private static String huDerivatives = "C:\\workspaces\\hunglish-webapp\\trunk\\data\\jmorph\\derivatives.lst";
+	private static String huCompounds = "C:\\workspaces\\hunglish-webapp\\trunk\\data\\jmorph\\compound.lst";
+	private static int huRecursionDepth = 4;
+	private static String enAff = "C:\\workspaces\\hunglish-webapp\\trunk\\data\\jmorph\\en.aff";
+	private static String enDic = "C:\\workspaces\\hunglish-webapp\\trunk\\data\\jmorph\\en.dic";
+	private static int enRecursionDepth = 2;
+	// private static String testDataSenHu =
+	// "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/test-data-sen.txt";
+	// private static String testDataQueryHu =
+	// "/Users/daniel/experiments/hunglish-webapp/spring-lucene-jmorph-skeleton/resources-lang/test-data.txt";
+	private static String testDataQueryHu = "C:\\work\\hunmorph\\root-test\\resources-lang\\test-data.txt";
 	public static String testDataEncodingHu = "UTF-8";
 	List<String> huQueries;
-	
+
 	private Analyzer analyzer;
 	private Directory directory;
 	private IndexWriter iwriter;
 	private IndexSearcher isearcher;
-	
+
 	public static String huFieldName = "left";
-	//public static String huStemmedFieldName = "left_stemmed";
+	// public static String huStemmedFieldName = "left_stemmed";
 	public static String enFieldName = "right";
-	//public static String enStemmedFieldName = "right_stemmed";
-	
+	// public static String enStemmedFieldName = "right_stemmed";
+
 	Analyser huAnalyser;
 	Analyser enAnalyser;
 
-	
 	Map<String, LemmatizerWrapper> lemmatizerMap;
-	
+
 	/**
 	 * @param args
 	 * @throws IOException
 	 * @throws LockObtainFailedException
 	 * @throws CorruptIndexException
 	 * @throws ParseException
-	 * @throws net.sf.jhunlang.jmorph.parser.ParseException 
-	 * @throws InstantiationException 
-	 * @throws IllegalAccessException 
+	 * @throws net.sf.jhunlang.jmorph.parser.ParseException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
 	 */
 	public static void main(String[] args) throws CorruptIndexException,
-			LockObtainFailedException, IOException, ParseException, IllegalAccessException, InstantiationException, net.sf.jhunlang.jmorph.parser.ParseException {
+			LockObtainFailedException, IOException, ParseException,
+			IllegalAccessException, InstantiationException,
+			net.sf.jhunlang.jmorph.parser.ParseException {
 
 		HungarianLuceneTester instance = new HungarianLuceneTester();
 		instance.justDoIt();
 	}
 
-	
 	private void justDoIt() throws CorruptIndexException,
-			LockObtainFailedException, IOException, ParseException, IllegalAccessException, InstantiationException, net.sf.jhunlang.jmorph.parser.ParseException {
+			LockObtainFailedException, IOException, ParseException,
+			IllegalAccessException, InstantiationException,
+			net.sf.jhunlang.jmorph.parser.ParseException {
 
 		huQueries = getLines(testDataQueryHu, testDataEncodingHu);
 		initLemmatizer();
-		
-		
-		//analyzer = new StandardAnalyzer(Version.LUCENE_30);
-		
-		System.out.println("------init done---------------");		
+
+		// analyzer = new StandardAnalyzer(Version.LUCENE_30);
+
+		System.out.println("------init done---------------");
 		analyzer = new StemmerAnalyzer(Version.LUCENE_CURRENT, lemmatizerMap);
-		
+
 		// Store the index in memory:
 		directory = new RAMDirectory();
 		// To store an index on disk, use this instead:
@@ -113,49 +132,50 @@ public class HungarianLuceneTester {
 				new IndexWriter.MaxFieldLength(25000));
 		Document doc = new Document();
 		String enText = "This is the text to be indexed.";
-		//List<String> huSens = getLines(testDataSenHu, testDataEncodingHu);
+		// List<String> huSens = getLines(testDataSenHu, testDataEncodingHu);
 		doc.add(new Field(enFieldName, enText, Field.Store.YES,
 				Field.Index.ANALYZED));
 		doc.add(new Field(huFieldName, huQueries.get(0), Field.Store.YES,
 				Field.Index.ANALYZED));
-		
+
 		iwriter.addDocument(doc);
 		iwriter.close();
 		System.out.println("------indexing done---------------");
 
 		// Now search the index:
 		isearcher = new IndexSearcher(directory, true); // read-only=true
-		
+
 		query(enFieldName, "indexed");
 		query(enFieldName, "index");
-				
-		for (String query : huQueries){
-			query(huFieldName, query);	
+
+		for (String query : huQueries) {
+			query(huFieldName, query);
 		}
-		
+
 		isearcher.close();
 		directory.close();
 
 	}
 
-	private void query(String fieldName, String term) throws ParseException, IOException{
+	private void query(String fieldName, String term) throws ParseException,
+			IOException {
 		QueryParser parser = new QueryParser(Version.LUCENE_30, fieldName,
-				analyzer);		
+				analyzer);
 		Query query = parser.parse(term);
 		ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
-System.out.println("searchd:"+term+";hits.length:" + hits.length);	
-		//org.junit.Assert.assertEquals(1, hits.length);
+		System.out.println("searchd:" + term + ";hits.length:" + hits.length);
+		// org.junit.Assert.assertEquals(1, hits.length);
 		// Iterate through the results:
 		for (int i = 0; i < hits.length; i++) {
 			Document hitDoc = isearcher.doc(hits[i].doc);
-			System.out.println("hitDoc.get(fieldname):"
-					+ hitDoc.get(fieldName));
-			//org.junit.Assert.assertEquals(enText,
-			//		hitDoc.get(enFieldName));
+			System.out
+					.println("hitDoc.get(fieldname):" + hitDoc.get(fieldName));
+			// org.junit.Assert.assertEquals(enText,
+			// hitDoc.get(enFieldName));
 		}
 
 	}
-	
+
 	private void initHuAnalyser() throws IOException, IllegalAccessException,
 			InstantiationException,
 			net.sf.jhunlang.jmorph.parser.ParseException {
@@ -184,30 +204,31 @@ System.out.println("searchd:"+term+";hits.length:" + hits.length);
 		enAnalyser = jf.build(new Definition[] { affixDef, dicDef });
 	}
 
-	public void initLemmatizer() throws IOException, IllegalAccessException, InstantiationException, ParseException, net.sf.jhunlang.jmorph.parser.ParseException{
+	public void initLemmatizer() throws IOException, IllegalAccessException,
+			InstantiationException, ParseException,
+			net.sf.jhunlang.jmorph.parser.ParseException {
 		initHuAnalyser();
 		initEnAnalyser();
-		AnalyserControl acHu = new AnalyserControl(AnalyserControl.ALL_COMPOUNDS);
+		AnalyserControl acHu = new AnalyserControl(
+				AnalyserControl.ALL_COMPOUNDS);
 		AnalyserContext analyserContextHu = new AnalyserContext(acHu);
 		boolean huStripDerivates = true;
 		Lemmatizer huLemmatizer = new net.sf.jhunlang.jmorph.lemma.LemmatizerImpl(
-				huAnalyser, huStripDerivates,
-				analyserContextHu);
-		
-		
-		AnalyserControl acRight = new AnalyserControl(AnalyserControl.ALL_COMPOUNDS);
+				huAnalyser, huStripDerivates, analyserContextHu);
+
+		AnalyserControl acRight = new AnalyserControl(
+				AnalyserControl.ALL_COMPOUNDS);
 		AnalyserContext analyserContextRight = new AnalyserContext(acRight);
 		boolean enStripDerivates = true;
 		Lemmatizer enLemmatizer = new net.sf.jhunlang.jmorph.lemma.LemmatizerImpl(
-						enAnalyser, enStripDerivates,
-						analyserContextRight);
-	
+				enAnalyser, enStripDerivates, analyserContextRight);
+
 		System.out.println("en lemmatizer created---------------");
 		testLemmatizer(enLemmatizer, "indexed");
 		testLemmatizer(enLemmatizer, "tests");
-		
+
 		lemmatizerMap = new HashMap<String, LemmatizerWrapper>();
-		
+
 		LemmatizerWrapper huLemmatizerWrapper = new LemmatizerWrapper();
 		huLemmatizerWrapper.setLemmatizer(huLemmatizer);
 		huLemmatizerWrapper.setReturnOOVOrig(false);
@@ -216,52 +237,52 @@ System.out.println("searchd:"+term+";hits.length:" + hits.length);
 		lemmatizerMap.put(huFieldName, huLemmatizerWrapper);
 
 		System.out.println("hu lemmatizer created--------------");
-		
-		for (String query : huQueries){
-			testLemmatizer(huLemmatizer, query);	
+
+		if (huQueries != null) {
+			for (String query : huQueries) {
+				testLemmatizer(huLemmatizer, query);
+			}
 		}
-				
+
 		LemmatizerWrapper enLemmatizerWrapper = new LemmatizerWrapper();
 		enLemmatizerWrapper.setLemmatizer(enLemmatizer);
 		enLemmatizerWrapper.setReturnOOVOrig(false);
 		enLemmatizerWrapper.setReturnOrig(true);
 		enLemmatizerWrapper.setReturnPOS(false);
-		lemmatizerMap.put(enFieldName, enLemmatizerWrapper);		
+		lemmatizerMap.put(enFieldName, enLemmatizerWrapper);
 	}
-	
-	private void testLemmatizer(Lemmatizer lemmatizer, String term){
-		System.out.println("stems for:"+term);
-		List<Lemma> lemmas = lemmatizer.lemmatize(term);		
-		for (Lemma lemma : lemmas){
+
+	private void testLemmatizer(Lemmatizer lemmatizer, String term) {
+		System.out.println("stems for:" + term);
+		List<Lemma> lemmas = lemmatizer.lemmatize(term);
+		for (Lemma lemma : lemmas) {
 			System.out.println(lemma.getWord());
 		}
-		
+
 	}
 
-	private static List<String> getLines(String path, String encoding) throws FileNotFoundException{
+	private static List<String> getLines(String path, String encoding)
+			throws FileNotFoundException {
 		List<String> result = new ArrayList<String>();
 		Scanner scanner = new Scanner(new File(path), encoding);
-	    try {
-	      //first use a Scanner to get each line
-	      while ( scanner.hasNextLine() ){
-	        result.add( scanner.nextLine() );
-	      }
-	    }
-	    finally {
-	      //ensure the underlying stream is always closed
-	      scanner.close();
-	    }
-	    return result;
+		try {
+			// first use a Scanner to get each line
+			while (scanner.hasNextLine()) {
+				result.add(scanner.nextLine());
+			}
+		} finally {
+			// ensure the underlying stream is always closed
+			scanner.close();
+		}
+		return result;
 	}
-
 
 	public Map<String, LemmatizerWrapper> getLemmatizerMap() {
 		return lemmatizerMap;
 	}
 
-
 	public void setLemmatizerMap(Map<String, LemmatizerWrapper> lemmatizerMap) {
 		this.lemmatizerMap = lemmatizerMap;
 	}
-	
+
 }
