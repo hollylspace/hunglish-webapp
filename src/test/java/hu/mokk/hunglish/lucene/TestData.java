8b105fa38947714ac2824763a23d630cd5bd2a16
varga.daniel <varga.daniel@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2010-05-07 22:28:06 +0000

    Létrehoztam egy pici beant (TestData), ami csak a testing.properties alapján feltöltött struct.

---
+++ src/test/java/hu/mokk/hunglish/lucene/TestData.java
@@ -0,0 +1,19 @@
+package hu.mokk.hunglish.lucene;
+
+// Just a struct referencing various test-specific resources.
+public class TestData {
+	public String testQueriesHu;
+	public String testQueriesEn;
+	public String testDataEncoding;
+
+	public void setTestQueriesHu(String testQueriesHu) {
+		this.testQueriesHu = testQueriesHu;
+	}
+	public void setTestQueriesEn(String testQueriesEn) {
+		this.testQueriesEn = testQueriesEn;
+	}
+	public void setTestDataEncoding(String testDataEncoding) {
+		this.testDataEncoding = testDataEncoding;
+	}
+
+}
