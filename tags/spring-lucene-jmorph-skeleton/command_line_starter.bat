bf0360439cbb1ef12d61a145aacace2adc0a9cc3
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2010-10-21 19:14:59 +0000

    command line interface for the harness pipeline this will be the last program executed by src/main/python/control_harness.py

--- tags/spring-lucene-jmorph-skeleton/command_line_starter.bat
+++ tags/spring-lucene-jmorph-skeleton/command_line_starter.bat
@@ -1,13 +1,13 @@
-rem  java -Xmx1500M -classpath lib:hunglish-0.1.0.jar:. hu.mokk.hunglish.lucene.Launcher
+rem  java -Xmx1500M -classpath lib/*:hunglish-0.1.0.jar:. hu.mokk.hunglish.lucene.Launcher
 java -Xmx1500M -classpath lib\*;hunglish-0.1.0.jar;. hu.mokk.hunglish.lucene.Launcher
 
 rem ***************************
 rem TODO ezt bele kell rakni apomba
 rem 1) a class-okat egy jar-ba
-rem 2) jar mellé lib konyvtárba a libeket
-rem 3) jar mellé META-INF könyntárba a persitance xml
-rem 4) jar mellé resources-lang könyvtárba a resource-okat
-rem 5) a jar mellé az appContext-et és a *.properties-t
-rem 6) az Appcontextban javítani
+rem 2) jar mellï¿½ lib konyvtï¿½rba a libeket
+rem 3) jar mellï¿½ META-INF kï¿½nyntï¿½rba a persitance xml
+rem 4) jar mellï¿½ resources-lang kï¿½nyvtï¿½rba a resource-okat
+rem 5) a jar mellï¿½ az appContext-et ï¿½s a *.properties-t
+rem 6) az Appcontextban javï¿½tani
 rem 6.1) 	<context:property-placeholder location="classpath*:*.properties" />
 rem 6.2) 	<bean id="multipartResolver" ---> ezt ki kell kommentezni
