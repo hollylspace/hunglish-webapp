f2ed2fa0fa560786f74aaea7dd2b97a1fe99558f
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2011-01-15 18:52:53 +0000

    spring did not inject beans into Quartz job

--- tags/spring-lucene-jmorph-skeleton/src/main/java/hu/mokk/hunglish/job/UploadJob.java
+++ tags/spring-lucene-jmorph-skeleton/src/main/java/hu/mokk/hunglish/job/UploadJob.java
@@ -8,29 +8,38 @@ import org.apache.commons.logging.LogFactory;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UploadJob implements Job {
 	private Log log = LogFactory.getLog(UploadJob.class);
-	
-	@Autowired
-	private Indexer indexer;
-	
-	@Autowired
-	private Searcher searcher;
+
+	// @ Autowired
+	// private Indexer indexer;
+
+	// @ Autowired
+	// private Searcher searcher;
+
+	private ApplicationContext getContext() {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "applicationContext.xml" });
+		return context;
+	}
 
 	@Override
 	public void execute(JobExecutionContext jc) throws JobExecutionException {
 		try {
+			ApplicationContext context = getContext();
+			Indexer indexer = (Indexer) context.getBean("indexer");
+			Searcher searcher = (Searcher) context.getBean("searcher");
+
 			log.debug("Executing upload processing job ... ");
-			if (indexer != null && searcher != null){
+			if (indexer != null && searcher != null) {
 				SystemCall.execute(indexer.getUploadJobPath());
 				indexer.indexAll();
-				searcher.reInitSearcher();				
+				searcher.reInitSearcher();
 			} else {
-				log.error("WTF, indexer or searcher is null! Spring, why do not you inject this !?");
-				SystemCall.execute(null);
+				log.error("WTF, indexer or searcher is null! ");
 			}
 			log.debug("Executed upload processing job!");
 		} catch (Exception e) {
