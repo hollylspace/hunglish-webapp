bcb8637e9be42d9f1ef889da0159eb5083d82cc6
attila.balogh@gmail.com <attila.balogh@gmail.com@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2011-03-01 22:43:13 +0000

    backup before moving 'spring-lucene-jmorph-skeleton' to trunk

---
+++ tags/trunk.backup/src/java/lucenedemo/PartOfSpeechAttribute.java
@@ -0,0 +1,13 @@
+package lucenedemo;
+
+import org.apache.lucene.util.Attribute;
+
+public interface PartOfSpeechAttribute extends Attribute {
+    public static enum PartOfSpeech {
+      Noun, Verb, Adjective, Adverb, Pronoun, Preposition, Conjunction, Article, Unknown
+    }
+  
+    public void setPartOfSpeech(PartOfSpeech pos);
+  
+    public PartOfSpeech getPartOfSpeech();
+  }
