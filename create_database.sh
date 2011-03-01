8629483c1fd5e2049fe1a06aef535f063311d7e0
attila.balogh <attila.balogh@gmail.com>
2011-03-01 23:19:05 +0000

    #46

---
+++ create_database.sh
@@ -0,0 +1,19 @@
+echo "CREATE USER 'hunglish'@'localhost' IDENTIFIED BY 'sw6x2the';" | mysql -uroot -psw6x2the 
+
+#DROP DATABASE IF EXISTS hunglishwebapp;
+#CREATE DATABASE hunglishwebapp CHARACTER SET 'utf8';
+
+echo "DROP DATABASE IF EXISTS hunglishwebapp;" | mysql -uroot -psw6x2the 
+
+echo "create database hunglishwebapp default character set = UTF8;" | mysql -uroot -psw6x2the 
+
+echo "GRANT ALL PRIVILEGES ON *.* TO 'hunglish'@'localhost' WITH GRANT OPTION;" | mysql -uroot -psw6x2the 
+
+cat create.sql | mysql -uroot -psw6x2the --default-character-set=utf8 hunglishwebapp
+
+cat create.sql | mysql -uhunglish -psw6x2the --default-character-set=utf8 hunglishwebapp
+
+# minidump /big3/Work/HunglishMondattar/hunglish-webapp/src/main/python/mini.mysqldump.beforeindex
+
+----
+mysqldump -uroot -psw6x2the --default-character-set=utf8 --no-data hunglishwebapp > hunglishwebapp.sql
