60c9eccd3c4cdb558f46c0eb061bc930a59a2798
attila.balogh <attila.balogh@gmail.com>
2011-03-01 22:55:27 +0000

    #46

---
+++ command_line_starter.bat
@@ -0,0 +1,20 @@
+rem  java -Xmx1500M -classpath lib/*:hunglish-0.1.0.jar:. hu.mokk.hunglish.lucene.Launcher
+java -Xmx1500M -classpath lib\*;hunglish-0.1.0.jar;. hu.mokk.hunglish.lucene.Launcher
+
+rem ***************************
+rem TODO ezt bele kell rakni apomba
+rem 1) a class-okat egy jar-ba
+rem 2) jar mell� lib konyvt�rba a libeket
+rem 3) jar mell� META-INF k�nynt�rba a persitance xml
+rem 4) jar mell� resources-lang k�nyvt�rba a resource-okat
+rem 5) a jar mell� az appContext-et �s a *.properties-t
+rem 6) az Appcontextban jav�tani
+rem 6.1) 	<context:property-placeholder location="classpath*:*.properties" />
+rem 6.2) 	<bean id="multipartResolver" ---> ezt ki kell kommentezni
+
+----------------------------
+rem maven war build
+rem 1) edit database.properties
+rem 2) copy resources-lang
+rem 3) create dirs: fileUpload, hunglishIndex, hunglishIndexTmp
+rem 4) copy j-morph jar into libs
