67dd215c0abf1d26ffe2c53abc3030fa164558fc
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2011-03-17 23:38:35 +0000

    Issue 44

--- trunk/src/main/java/hu/mokk/hunglish/job/SystemCall.java
+++ trunk/src/main/java/hu/mokk/hunglish/job/SystemCall.java
@@ -7,16 +7,12 @@ import org.apache.commons.logging.LogFactory;
 
 public class SystemCall{
 	
-	private static String defaultCommand = "/big3/Work/HunglishMondattar/deployment/harness_cronjob.sh";
-	// private static String defaultCommand = "/bin/bash /big3/Work/HunglishMondattar/hunglish-webapp/src/main/python/harness_cronjob.sh";
-	
 	private static Log logger = LogFactory.getLog(SystemCall.class);
 	
     public static int execute(String command) {
     	int result = 0;
     	if (command == null){
-    		logger.error("Command is null, using default command: "+defaultCommand);
-    		command = defaultCommand;
+    		throw new RuntimeException("harness command is null");
     	}
         Runtime r = Runtime.getRuntime();
 
@@ -28,10 +24,13 @@ public class SystemCall{
                 }
             } catch (InterruptedException e) {
             	logger.error("System call interupted.", e);
+            	//TODO
             } finally {
+            	//TODO
             }
         } catch (IOException e) {
         	logger.error("IO error in system call.", e);
+        	//TODO
         }
         return result;
      }
