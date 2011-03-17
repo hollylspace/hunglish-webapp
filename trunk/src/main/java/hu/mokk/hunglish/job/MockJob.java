cd8569b3f8f9a6302b8219bd4e380b12d6f5e5ed
attila.balogh@gmail.com <attila.balogh@gmail.com@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2011-03-17 13:50:51 +0000

    Issue 43
    http://code.google.com/p/hunglish-webapp/issues/detail?id=43

--- trunk/src/main/java/hu/mokk/hunglish/job/MockJob.java
+++ trunk/src/main/java/hu/mokk/hunglish/job/MockJob.java
@@ -10,27 +10,63 @@ import org.apache.commons.logging.LogFactory;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.StatefulJob;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 public class MockJob { // extends QuartzJobBean implements StatefulJob {
 	transient private static Log logger = LogFactory.getLog(MockJob.class);
 	private static int HARNESS_SUCCESS_CODE = 0;
-	
+
+	@Autowired
+	private TaskExecutor singleThreadExecutor;
+
+	//@Scheduled(cron = "0 * * * * ?")
+	public void doItPeriodically() {
+		logger.info("trying to run doSomeAction() periodically ...");
+		MyTask myTask = new MyTask();
+		singleThreadExecutor.execute(myTask);
+	}
+
+	private class MyTask implements Runnable {
+
+		@Override
+		public void run() {
+			doSomeAction();
+
+		}
+
+	}
+
+	public void doItOnDemand() {
+		logger.info("trying to run doSomeAction() on-demand ...");
+		MyTask myTask = new MyTask();
+		singleThreadExecutor.execute(myTask);
+	}
+
 	public void doSomeAction() {
 
 		logger.info("mokk-job doing some action ...");
-		
+
 		try {
 			Indexer indexer = Indexer.getInstance();
 			Searcher searcher = Searcher.getInstance();
 			logger.debug("Executing upload processing job ... ");
 			if (indexer != null && searcher != null) {
-				//if harness exit status is not OK, then do not start indexer
-				//I just put it here, so when, we change this to asynchronous processing,  
-				//then this logic should be implemented
-				int exitCode = SystemCall.execute(indexer.getUploadJobPath());
-				logger.info("harness job finished, exit code:"+exitCode);
-				if (HARNESS_SUCCESS_CODE == exitCode){
+				// if harness exit status is not OK, then do not start indexer
+				// I just put it here, so when, we change this to asynchronous
+				// processing,
+				// then this logic should be implemented
+				// TODO remove this
+				logger.info("waiting...");
+				Thread.sleep(15000);
+				int exitCode = 0;
+				// int exitCode =
+				// SystemCall.execute(indexer.getUploadJobPath());
+				logger.info("harness job finished, exit code:" + exitCode);
+				if (HARNESS_SUCCESS_CODE == exitCode) {
 					indexer.indexAll();
 					searcher.reInitSearcher();
 				}
@@ -41,17 +77,15 @@ public class MockJob { // extends QuartzJobBean implements StatefulJob {
 		} catch (Exception e) {
 			logger.error("Failed to execute upload processing job.", e);
 		}
-		
-		
+
 	}
 
 	/*
-	@Override
-	protected void executeInternal(JobExecutionContext arg0)
-			throws JobExecutionException {
-
-		logger.info("job running...");
-
-	}
-	*/
+	 * @Override protected void executeInternal(JobExecutionContext arg0) throws
+	 * JobExecutionException {
+	 * 
+	 * logger.info("job running...");
+	 * 
+	 * }
+	 */
 }
