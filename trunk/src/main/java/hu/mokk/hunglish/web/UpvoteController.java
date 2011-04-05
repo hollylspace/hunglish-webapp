902eef952f5b3556043acafc247f7309d6fddee6
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2011-04-05 10:32:30 +0000

    Issue 9

--- trunk/src/main/java/hu/mokk/hunglish/web/UpvoteController.java
+++ trunk/src/main/java/hu/mokk/hunglish/web/UpvoteController.java
@@ -1,24 +1,42 @@
 package hu.mokk.hunglish.web;
 
 import hu.mokk.hunglish.domain.Bisen;
+import hu.mokk.hunglish.job.MockJob;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 public class UpvoteController {
 	transient private static Log logger = LogFactory.getLog(UpvoteController.class);
+
+	@Autowired
+	private MockJob mockJob;
+
+	public void setMockJob(MockJob mockJob) {
+		this.mockJob = mockJob;
+	}
+
 	
-	@Transactional
-	public void upvote(Long id){
-		Bisen.upvote(id);
-		logger.info("upvoted:"+id);
+	private void startUploadJob() throws SchedulerException {
+		logger.info("trying to run job on demand...");
+		mockJob.doItOnDemand();
 	}
 	
 	@Transactional
-	public void downvote(Long id){
+	public void upvote(Long id) throws SchedulerException{
+		Bisen.upvote(id);
+		logger.info("upvoted:"+id);
+		startUploadJob();		
+	}
+	
+	@Transactional
+	public void downvote(Long id) throws SchedulerException{
 		Bisen.downvote(id);
 		logger.info("downvoted:"+id);
+		startUploadJob();
 	}
 	
 }
