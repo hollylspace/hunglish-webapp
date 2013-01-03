d3d0df19269a8addc5bbbdf229ff1f1a2a8ece94
bpgergo <bpgergo@88fcd7cc-af73-11de-bed4-89e45268dd3d>
2013-01-03 12:28:26 +0000

    delete old vote button gifs

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,4 +1,9 @@
+BACKUPDIR=/home/bpgergo/workspaces/hunglish2/hunglish/backup
 WEBAPPDIR=/var/lib/tomcat6/webapps
+
+sudo rm -rf $BACKUPDIR/*
+sudo cp $WEBAPPDIR/ROOT.war $BACKUPDIR
+
 sudo /etc/init.d/tomcat6 stop
 mvn clean package
 sudo rm -rf $WEBAPPDIR/ROOT
